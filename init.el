6f667d1c8bd4340f1db5e56dec2e118766136f21
xyplayman <xyplayman@outlook.com>
2019-09-13 16:18:42 +0800

    init swiper

--- init.el
+++ init.el
@@ -1,10 +1,21 @@
 (when (eq window-system 'ns)
   (set-frame-font "Menlo-20" nil t))
 
-(add-to-list 'load-path "~/.emacs.d/extensions/better-defaults")
+(add-to-list 'load-path "~/.emacs.d/extensions/better-defaults/")
 (require 'better-defaults)
 
-(add-to-list 'load-path "~/.emacs.d/extensions/exec-path-from-shell")
+(add-to-list 'load-path "~/.emacs.d/extensions/exec-path-from-shell/")
 (require 'exec-path-from-shell)
 (when (memq window-system '(mac ns))
   (exec-path-from-shell-initialize))
+
+(add-to-list 'load-path "~/.emacs.d/extensions/swiper/")
+(require 'ivy)
+(require 'counsel)
+(ivy-mode 1)
+(setq ivy-use-virtual-buffers t)
+(setq ivy-count-format "(%d/%d) ")
+(setq enable-recursive-minibuffers t)
+(global-set-key (kbd "C-s") 'swiper)
+(global-set-key (kbd "M-x") 'counsel-M-x)
+(global-set-key (kbd "C-x C-f") 'counsel-find-file)
